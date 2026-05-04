af345564d0c6cb52ff5e526bf0ec730fb6aa486c
habibasaker <habiba.amr.osama@gmail.com>
2026-05-04 22:03:14 +0200

    Update admin.h

--- include/admin.h
+++ include/admin.h
@@ -1,12 +1,12 @@
 #ifndef ADMIN_H
 #define ADMIN_H
-
 #include "user.h"
+#include <string>
+using namespace std;
 
 class Admin : public User
 {
 public:
     Admin(int id, string name, string email, string password);
 };
-
 #endif
