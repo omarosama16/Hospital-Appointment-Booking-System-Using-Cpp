e794b82eecb228eab08791f4372acfff52749972
Omar Osama <omarossamaa2006@gmail.com>
2026-05-04 17:08:54 +0300

    Convert tests to use Google Test framework
    Replaced main function with Google Test framework for unit testing.

--- tests/test_sample.cpp
+++ tests/test_sample.cpp
@@ -1,22 +1,7 @@
-#include <iostream>
-#include <cassert>
-using namespace std;
-int add(int a, int b) {
-    return a + b;
-}
-bool isEven(int x) {
-    return x % 2 == 0;
-}
-int main() {
+#include <gtest/gtest.h>
+#include "Doctor.h"
 
-    assert(add(2, 3) == 5);
-    assert(add(0, 0) == 0);
-    assert(add(-1, 1) == 0);
-
-    assert(isEven(2) == true);
-    assert(isEven(3) == false);
-
-    cout << "All tests passed successfully!" << endl;
-
-    return 0;
+TEST(DoctorTest, NameCheck) {
+    Doctor d("Omar");
+    EXPECT_EQ(d.name, "Omar");
 }
