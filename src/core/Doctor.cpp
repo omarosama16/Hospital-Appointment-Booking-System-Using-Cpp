91dcc0a5a1e91e98ce53c652a5fd24fe208e0307
habibasaker <habiba.amr.osama@gmail.com>
2026-05-04 22:06:17 +0200

    Update Doctor.cpp

--- src/core/Doctor.cpp
+++ src/core/Doctor.cpp
@@ -1,17 +1,11 @@
 #include "Doctor.h"
-#include <iostream>
-
 using namespace std;
 
 Doctor::Doctor(int id, string n, string e, string p, string s)
-    : User(id, n, e, p, "doctor")
-{
-    specialization = s;
-}
+    : User(id, n, e, p, "doctor"), specialization(s) {}
 
-string Doctor::getSpecialization() const { return specialization; }
-
-vector<string> Doctor::getAvailability() const { return availability; }
+string         Doctor::getSpecialization() const { return specialization; }
+vector<string> Doctor::getAvailability()   const { return availability; }
 
 void Doctor::addAvailability(string time)
 {
