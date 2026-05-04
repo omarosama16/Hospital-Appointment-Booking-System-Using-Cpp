76112e6e6be1122924096223ea8aedeafcacac33
Omar Osama <omarossamaa2006@gmail.com>
2026-05-04 17:14:06 +0300

    Update DoctorTest to use new naming conventions

--- tests/test_sample.cpp
+++ tests/test_sample.cpp
@@ -4,7 +4,6 @@
 TEST(DoctorTest, ConstructorAndGetters) {
     Doctor d(1, "Omar", "omar@test.com", "1234", "cardiology");
 
-    EXPECT_EQ(d.getName(), "Omar");
-
-    EXPECT_EQ(d.getId(), 1);
+    EXPECT_EQ(d.get_name(), "Omar");
+    EXPECT_EQ(d.get_id(), 1);
 }
