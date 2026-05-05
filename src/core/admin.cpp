db375c534685dd1b32bc93a3b5948f02e765330d
Omar Osama <omarossamaa2006@gmail.com>
2026-05-05 14:39:44 +0300

    Delete src directory

--- src/core/admin.cpp
+++
@@ -1,5 +0,0 @@
-#include "admin.h"
-using namespace std;
-
-Admin::Admin(int id, string name, string email, string password)
-    : User(id, name, email, password, "Admin") {}
--- src/core/patient.cpp
+++
@@ -1,9 +0,0 @@
-#include "patient.h"
-using namespace std;
-
-patient::patient(int i, string n, string e, string p, string pa)
-    : User(i, n, e, p, "Patient"), phone(pa) {}
-
-string patient::get_phone() const { return phone; }
-
-void patient::set_phone(string p) { phone = p; }
