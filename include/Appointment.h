26d4415856fc2da5140c64ce3e8945f90c394575
habibasaker <habiba.amr.osama@gmail.com>
2026-05-07 14:06:37 +0200

    Refactor Appointment class and add AppointmentInfo struct

--- include/Appointment.h
+++ include/Appointment.h
@@ -1,36 +1,29 @@
-#ifndef APPOINTMENT_H
-#define APPOINTMENT_H
-
-#include <string>
-
-class Appointment
-{
-private:
-    int appointmentid;
-    int patientid;
-    int doctorid;
+struct AppointmentInfo {
     std::string patientname;
     std::string doctorname;
     std::string date;
     std::string time;
     std::string status;
+};
 
+class Appointment {
 public:
     Appointment();
-    Appointment(int a, int p, int d, std::string pa, std::string doc,
-                std::string da, std::string t, std::string s);
+    Appointment(int a, int p, int d, const AppointmentInfo& info); // 4 params, all issues fixed
 
     void cancel();
     void complete();
     void print_row() const;
 
     int get_AppointmentId() const;
-    int get_PatientId() const;
-    int get_DoctorId() const;
+    int get_PatientId()     const;
+    int get_DoctorId()      const;
 
-    std::string get_Date() const;
-    std::string get_Time() const;
+    std::string get_Date()   const;
+    std::string get_Time()   const;
     std::string get_Status() const;
-};
 
-#endif
+private:
+    int appointmentid, patientid, doctorid;
+    std::string patientname, doctorname, date, time, status;
+};
