d113370066b3611a924efd7750c8ab8f5b5b02e0
habibasaker <habiba.amr.osama@gmail.com>
2026-05-07 14:27:52 +0200

    Update Appointment.h

--- include/Appointment.h
+++ include/Appointment.h
@@ -1,29 +1,33 @@
+#pragma once
+
+#include <string>
+
 struct AppointmentInfo {
-    std::string patientname;
-    std::string doctorname;
+    std::string patientName;
+    std::string doctorName;
     std::string date;
     std::string time;
     std::string status;
 };
 
 class Appointment {
+private:
+    int appointmentId;
+    int patientId;
+    int doctorId;
+
+    AppointmentInfo info;
+
 public:
     Appointment();
-    Appointment(int a, int p, int d, const AppointmentInfo& info); // 4 params, all issues fixed
+    Appointment(int appointmentId, int patientId, int doctorId, const AppointmentInfo& info);
 
     void cancel();
     void complete();
+
     void print_row() const;
 
-    int get_AppointmentId() const;
-    int get_PatientId()     const;
-    int get_DoctorId()      const;
-
-    std::string get_Date()   const;
-    std::string get_Time()   const;
+    std::string get_Date() const;
+    std::string get_Time() const;
     std::string get_Status() const;
-
-private:
-    int appointmentid, patientid, doctorid;
-    std::string patientname, doctorname, date, time, status;
 };
