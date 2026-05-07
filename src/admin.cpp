008f2637785f4438f5224d78929ecdfe79db6b79
mohamedsalah09 <mohamedsalahrohaim09@gmail.com>
2026-05-07 14:09:58 +0300

    last

--- src/admin.cpp
+++ src/admin.cpp
@@ -1,4 +1,8 @@
 #include "admin.h"
 
 Admin::Admin(int id, std::string name, std::string email, std::string password)
-    : User(id, name, email, password, "admin") {}
+    : User(id, name, email, password, "admin")
+{
+
+    set_role("admin");
+}
--- tests/admintest.cpp
+++ tests/admintest.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "admin.h"
 
-TEST(AdminTest, Constructor)
+TEST(AdminTest, ConstructorBasic)
 {
     Admin a(1, "Omar", "omar@test.com", "1234");
 
@@ -12,10 +12,47 @@ TEST(AdminTest, Constructor)
     EXPECT_EQ(a.get_role(), "admin");
 }
 
-TEST(AdminTest, AuthenticateInheritsUser)
+TEST(AdminTest, AuthenticateSuccess)
 {
     Admin a(1, "Omar", "omar@test.com", "1234");
 
     EXPECT_TRUE(a.Authenticate("omar@test.com", "1234"));
-    EXPECT_FALSE(a.Authenticate("wrong", "1234"));
+}
+
+TEST(AdminTest, AuthenticateWrongEmail)
+{
+    Admin a(1, "Omar", "omar@test.com", "1234");
+
+    EXPECT_FALSE(a.Authenticate("wrong@test.com", "1234"));
+}
+
+TEST(AdminTest, AuthenticateWrongPassword)
+{
+    Admin a(1, "Omar", "omar@test.com", "1234");
+
+    EXPECT_FALSE(a.Authenticate("omar@test.com", "wrong"));
+}
+
+TEST(AdminTest, AuthenticateBothWrong)
+{
+    Admin a(1, "Omar", "omar@test.com", "1234");
+
+    EXPECT_FALSE(a.Authenticate("x", "y"));
+}
+
+TEST(AdminTest, EmptyCredentialsEdge)
+{
+    Admin a(1, "Omar", "omar@test.com", "1234");
+
+    EXPECT_FALSE(a.Authenticate("", ""));
+}
+
+TEST(AdminTest, InheritanceRoleFixed)
+{
+    Admin a(10, "AdminUser", "admin@mail.com", "admin123");
+
+    EXPECT_EQ(a.get_role(), "admin");
+
+    a.set_role("doctor");
+    EXPECT_EQ(a.get_role(), "doctor");
 }
