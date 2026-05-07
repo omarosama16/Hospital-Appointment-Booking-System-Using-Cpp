33b2b3627665b6a5e639d6177ebae4a184d06aaf
habibasaker <habiba.amr.osama@gmail.com>
2026-05-07 14:04:08 +0200

    Update Appointment.cpp

--- src/Appointment.cpp
+++ src/Appointment.cpp
@@ -3,17 +3,16 @@
 
 Appointment::Appointment() : appointmentid(0), patientid(0), doctorid(0), date(""), time(""), status("") {}
 
-Appointment::Appointment(int a, int p, int d, const std::string& pa, const std::string& doc,
-                         const std::string& da, const std::string& t, const std::string& s)
+Appointment::Appointment(int a, int p, int d, const AppointmentInfo& info)
 {
     appointmentid = a;
-    patientid = p;
-    doctorid = d;
-    patientname = pa;
-    doctorname = doc;
-    date = da;
-    time = t;
-    status = s;
+    patientid     = p;
+    doctorid      = d;
+    patientname   = info.patientname;
+    doctorname    = info.doctorname;
+    date          = info.date;
+    time          = info.time;
+    status        = info.status;
 }
 
 void Appointment::cancel() { status = "Cancelled"; }
@@ -36,9 +35,9 @@ void Appointment::print_row() const
 }
 
 int Appointment::get_AppointmentId() const { return appointmentid; }
-int Appointment::get_PatientId() const { return patientid; }
-int Appointment::get_DoctorId() const { return doctorid; }
+int Appointment::get_PatientId()     const { return patientid; }
+int Appointment::get_DoctorId()      const { return doctorid; }
 
-std::string Appointment::get_Date() const { return date; }
-std::string Appointment::get_Time() const { return time; }
+std::string Appointment::get_Date()   const { return date; }
+std::string Appointment::get_Time()   const { return time; }
 std::string Appointment::get_Status() const { return status; }
