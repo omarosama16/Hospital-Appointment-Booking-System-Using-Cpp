89f3d761de24f87dffcde7c04d26f78c8512f19c
habibasaker <habiba.amr.osama@gmail.com>
2026-05-07 14:39:30 +0200

    Update user.cpp

--- src/user.cpp
+++ src/user.cpp
@@ -9,14 +9,14 @@ User::User()
     role = "";
 }
 
-User::User(int i, std::string n, std::string e, std::string p, std::string r)
+User::User(int i, const std::string& n, const std::string& e, const std::string& p, const std::string& r)
     : id(i), name(n), email(e), password(p), role(r) {}
 
 void User::set_id(int i) { id = i; }
-void User::set_name(std::string n) { name = n; }
-void User::set_password(std::string p) { password = p; }
-void User::set_role(std::string r) { role = r; }
-void User::set_email(std::string e) { email = e; }
+void User::set_name(const std::string& n) { name = n; }
+void User::set_password(const std::string& p) { password = p; }
+void User::set_role(const std::string& r) { role = r; }
+void User::set_email(const std::string& e) { email = e; }
 
 int User::get_id() const { return id; }
 std::string User::get_name() const { return name; }
@@ -24,7 +24,7 @@ std::string User::get_email() const { return email; }
 std::string User::get_password() const { return password; }
 std::string User::get_role() const { return role; }
 
-bool User::Authenticate(std::string e, std::string p)
+bool User::Authenticate(const std::string& e, const std::string& p)
 {
     return email == e && password == p;
 }
