aca7a375ebec317f7ef20f2693d5f1671b4a7661
habibasaker <habiba.amr.osama@gmail.com>
2026-05-08 10:31:11 +0200

    Update Doctor.cpp

--- src/Doctor.cpp
+++ src/Doctor.cpp
@@ -1,6 +1,7 @@
 #include "Doctor.h"
 
-Doctor::Doctor(int i, std::string n, std::string e, std::string p, std::string s)
+Doctor::Doctor(int i, std::string_view n, std::string_view e,
+               std::string_view p, std::string_view s)
     : User(i, n, e, p, "doctor"), specialization(s) {}
 
 std::string Doctor::getSpecialization() const
@@ -13,9 +14,9 @@ std::vector<std::string> Doctor::getAvailability() const
     return availability;
 }
 
-void Doctor::addAvailability(std::string time)
+void Doctor::addAvailability(std::string_view time)
 {
-    availability.push_back(time);
+    availability.emplace_back(time);
 }
 
 void Doctor::clearAvailability()
