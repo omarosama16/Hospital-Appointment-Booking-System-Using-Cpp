fbc02b67bd13dcf98403af8ccb3a2c710fb8e460
habibasaker <habiba.amr.osama@gmail.com>
2026-05-08 10:34:22 +0200

    Update admin.cpp

--- src/admin.cpp
+++ src/admin.cpp
@@ -1,8 +1,6 @@
 #include "admin.h"
 
-Admin::Admin(int id, std::string name, std::string email, std::string password)
+Admin::Admin(int id, std::string_view name, std::string_view email, std::string_view password)
     : User(id, name, email, password, "admin")
 {
-
-    set_role("admin");
 }
