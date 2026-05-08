b5eb21e284444f4ff08efbb7d584f7adb76b0540
habibasaker <habiba.amr.osama@gmail.com>
2026-05-08 10:37:31 +0200

    Update Patient.h

--- include/Patient.h
+++ include/Patient.h
@@ -1,18 +1,18 @@
-#ifndef PATIENT_H
-#define PATIENT_H
+#pragma once
 
 #include "user.h"
+#include <string>
+#include <string_view>
 
 class Patient : public User
 {
+public:
+    Patient(int i, std::string_view n, std::string_view e,
+            std::string_view p, std::string_view ph);
+
+    void        set_phone(std::string_view p);
+    std::string get_phone() const;
+
 private:
     std::string phone;
-
-public:
-    Patient(int id, std::string name, std::string email, std::string password, std::string phone);
-
-    void set_phone(std::string p);
-    std::string get_phone() const;
 };
-
-#endif
