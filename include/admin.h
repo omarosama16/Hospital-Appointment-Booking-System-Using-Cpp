60d9d162e009d840f92095513c92de967c773353
habibasaker <habiba.amr.osama@gmail.com>
2026-05-08 10:34:04 +0200

    Update admin.h

--- include/admin.h
+++ include/admin.h
@@ -1,12 +1,10 @@
-#ifndef ADMIN_H
-#define ADMIN_H
+#pragma once
 
 #include "user.h"
+#include <string_view>
 
 class Admin : public User
 {
 public:
-    Admin(int id, std::string name, std::string email, std::string password);
+    Admin(int id, std::string_view name, std::string_view email, std::string_view password);
 };
-
-#endif
